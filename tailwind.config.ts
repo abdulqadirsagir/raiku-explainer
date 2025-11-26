d0bd99b05e5acb069ace0db7e5246e47c4a2c282
Abdulqadir Sagir <62840184+abdulqadirsagir@users.noreply.github.com>
2025-11-26 20:13:39 +0100

    Update Tailwind CSS config with proper syntax

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,11 @@
-content: [
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}", // <--- Note NO "src"
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
